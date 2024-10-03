b79aa66ef5310333fd7c24c63af40006f08903c1
priya <priya.biradar14@gmail.com>
2024-10-03 18:07:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+//importing the req modules
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set('view engine','ejs')
+app.use(express.static('public'))
+const {v4:uuidv4} = require("uuid")
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const {ExpressPeerServer} = require("peer");
+const peerServer = ExpressPeerServer(server,{
+    debug: true
+});
+
+app.use("/peerjs",peerServer);
+
+//defining routes
+app.get('/',(req,res) => {
+  
+  res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:room',(req,res) => {
+    res.render("room",{roomId: req.params.room})
+})
+
+//handling socket.io events
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+      socket.join(roomId);
+      setTimeout(()=>{
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+      }, 1000)
+    
+    socket.on("disconnect",() => {
+        console.log("User Disconnected");
+        io.emit("user-disconnected",userId)
+    })
+});
+  });
+
+//start server
+server.listen(process.env.PORT ||3030);
